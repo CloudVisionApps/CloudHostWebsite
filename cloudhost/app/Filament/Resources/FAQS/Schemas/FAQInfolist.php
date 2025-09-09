6c9056dae30b74e0c8aefa85010e9550f0ab1a0b
Bozhidar <bobicloudvision@gmail.com>
2025-09-09 19:56:03 +0300

    update

---
+++ cloudhost/app/Filament/Resources/FAQS/Schemas/FAQInfolist.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\FAQS\Schemas;
+
+use Filament\Schemas\Schema;
+
+class FAQInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
