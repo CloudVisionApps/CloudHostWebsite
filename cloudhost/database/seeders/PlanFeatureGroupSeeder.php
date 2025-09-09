58fe5af6f59792c70bd7eb228ed7627fa99025f7
Bozhidar <bobicloudvision@gmail.com>
2025-09-09 23:49:06 +0300

    update

--- cloudhost/database/seeders/PlanFeatureGroupSeeder.php
+++ cloudhost/database/seeders/PlanFeatureGroupSeeder.php
@@ -20,7 +20,7 @@ class PlanFeatureGroupSeeder extends Seeder
                 'name' => 'Сървър',
                 'slug' => 'server',
                 'description' => 'Спецификации на сървъра и хардуерни характеристики',
-                'icon' => 'server',
+                'icon' => 'fas-server',
                 'icon_color' => 'blue',
                 'sort_order' => 1,
             ],
@@ -28,7 +28,7 @@ class PlanFeatureGroupSeeder extends Seeder
                 'name' => 'Производителност',
                 'slug' => 'performance',
                 'description' => 'Оптимизация на производителността и скоростни функции',
-                'icon' => 'tachometer-alt',
+                'icon' => 'fas-tachometer-alt',
                 'icon_color' => 'green',
                 'sort_order' => 2,
             ],
@@ -36,7 +36,7 @@ class PlanFeatureGroupSeeder extends Seeder
                 'name' => 'Сигурност',
                 'slug' => 'security',
                 'description' => 'Функции за сигурност и архивиране',
-                'icon' => 'shield-alt',
+                'icon' => 'fas-shield-alt',
                 'icon_color' => 'red',
                 'sort_order' => 3,
             ],
@@ -44,7 +44,7 @@ class PlanFeatureGroupSeeder extends Seeder
                 'name' => 'Разработка',
                 'slug' => 'development',
                 'description' => 'Инструменти за разработка и среди',
-                'icon' => 'code',
+                'icon' => 'fas-code',
                 'icon_color' => 'purple',
                 'sort_order' => 4,
             ],
@@ -52,7 +52,7 @@ class PlanFeatureGroupSeeder extends Seeder
                 'name' => 'Бази данни',
                 'slug' => 'databases',
                 'description' => 'Функции за бази данни и инструменти за управление',
-                'icon' => 'database',
+                'icon' => 'fas-database',
                 'icon_color' => 'indigo',
                 'sort_order' => 5,
             ],
@@ -60,7 +60,7 @@ class PlanFeatureGroupSeeder extends Seeder
                 'name' => 'Имейл',
                 'slug' => 'email',
                 'description' => 'Имейл хостинг и функции за управление',
-                'icon' => 'envelope',
+                'icon' => 'fas-envelope',
                 'icon_color' => 'teal',
                 'sort_order' => 6,
             ],
@@ -68,14 +68,14 @@ class PlanFeatureGroupSeeder extends Seeder
                 'name' => 'WordPress Поддръжка',
                 'slug' => 'wordpress-support',
                 'description' => 'Услуги за поддръжка на WordPress и WooCommerce',
-                'icon' => 'wordpress',
+                'icon' => 'fab-wordpress',
                 'icon_color' => 'orange',
                 'sort_order' => 7,
             ],
         ];
 
         foreach ($planFeatureGroups as $planFeatureGroupData) {
-            PlanFeatureGroup::firstOrCreate(
+            PlanFeatureGroup::updateOrCreate(
                 ['name' => $planFeatureGroupData['name']],
                 $planFeatureGroupData
             );
